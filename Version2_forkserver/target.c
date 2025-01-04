7d376eef2af3731e8180930a82863bc21c20c35b
JaeHun Gou <106510018+P2GONE@users.noreply.github.com>
2025-01-05 00:08:16 +0900

    Update target.c

--- Version2_forkserver/target.c
+++ Version2_forkserver/target.c
@@ -2,24 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
 
 int main(int argc, char *argv[]) {
-    char buffer[100];
+    char buffer[20];
     
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <input>\n", argv[0]);
         return 1;
     }
 
-    strncpy(buffer, argv[1], 100);
-    buffer[99] = '\0'; // 안전하게 null-terminate
-
-    // 간단한 조건: 입력이 "crash"이면 강제 크래시
-    if (strcmp(buffer, "crash") == 0) {
-        printf("Crashing as per input...\n");
-        int *ptr = NULL;
-        *ptr = 42; // Segmentation Fault 유발
-    }
+    strcpy(buffer, argv[1]);
 
     printf("Received input: %s\n", buffer);
     return 0;
